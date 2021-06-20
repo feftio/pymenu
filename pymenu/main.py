4220d22cb8329a79efcb7eb930b1355dd55f5453
Lik Eduard <feft99@gmail.com>
2021-06-21 02:46:17 +0600

    func route return type added

--- pymenu/main.py
+++ pymenu/main.py
@@ -9,7 +9,7 @@ class PyMenu:
         pass
 
     # Used as decorator
-    def route(self, rule : str):
+    def route(self, rule : str) -> t.Callable:
         def decorator(action: t.Callable) -> t.Callable:
             self.actions_list.append(action.__name__)
             return action
