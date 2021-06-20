bfb592923693e6e1b9f5bba0b00fd4bf7f3df405
Lik Eduard <feft99@gmail.com>
2021-06-20 19:47:11 +0600

    sketch of the class PyMenu in main.py

---
+++ pymenu/main.py
@@ -0,0 +1,16 @@
+import typing as t
+
+
+class PyMenu:
+    actions_list: list = []
+
+    # Constructor
+    def __init__(self, *args, **kwargs):
+        pass
+
+    # Used as decorator
+    def route(self, rule : str):
+        def decorator(action: t.Callable) -> t.Callable:
+            self.actions_list.append(action.__name__)
+            return action
+        return decorator
