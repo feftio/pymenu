f5c3e3af973a3e1a4bea404e7936e16d118c5726
Lik Eduard <feft99@gmail.com>
2021-06-24 04:01:06 +0600

    testing Page and new decorator

--- examples/example.py
+++ examples/example.py
@@ -2,8 +2,22 @@ import pymenu
 
 menu = pymenu.PyMenu()
 
-@menu.route('/')
-def home():
+class HomePage(pymenu.Page):
     pass
 
-print(menu.actions_list)
+class OtherPage(pymenu.Page):
+    pass
+
+@menu.page
+def home():
+    print('It\'s home page.')
+    return HomePage()
+
+
+@menu.page
+def other():
+    print('It\'s other page.')
+    return OtherPage()
+
+
+menu.load('other')
