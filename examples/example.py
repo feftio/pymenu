8c856b1cff567d800da723f4990959828893a851
Lik Eduard <feft99@gmail.com>
2021-06-30 04:52:04 +0600

    example new approach using pymenu

--- examples/example.py
+++ examples/example.py
@@ -1,23 +1,32 @@
-import pymenu
-
-menu = pymenu.PyMenu()
-
-class HomePage(pymenu.Page):
-    pass
-
-class OtherPage(pymenu.Page):
-    pass
-
-@menu.page
-def home():
-    print('It\'s home page.')
-    return HomePage()
+from pymenu import PyMenu
+from pymenu.page import DefaultPage, SelectionPage
+import os
 
 
-@menu.page
-def other():
-    print('It\'s other page.')
-    return OtherPage()
+menu = PyMenu()
 
 
-menu.load('other')
+@menu.page(name='home')
+class HomeSelectionPage(SelectionPage):
+
+    @menu.item(output='1. Home.', triggers=['1'])
+    def home(self):
+        pass
+
+    @menu.item(output='2. Other.', triggers=['2'])
+    def other(self):
+        pass
+
+    @menu.item(output='3. Exit.', triggers=['3'])
+    def exit(self):
+        os.system('exit')
+
+
+@menu.page(name='other')
+class OtherPage(DefaultPage):
+
+    def show(self):
+        print("Other Page is showing...")
+
+
+menu.show('home')
