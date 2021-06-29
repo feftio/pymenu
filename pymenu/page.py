4771cc651b48a6964f38c0e5318c1c19b8a4f91c
Lik Eduard <feft99@gmail.com>
2021-06-30 04:54:26 +0600

    only page classes

--- pymenu/page.py
+++ pymenu/page.py
@@ -1,6 +1,28 @@
-class Page:
-    def __init__(self):
-        print(self.__class__.__name__)
+from __future__ import annotations
+import typing as t
+from abc import ABCMeta, abstractmethod
 
-    def load(self):
+
+class Page(metaclass=ABCMeta):
+    def __init__(self):
+        if hasattr(self, 'show') and callable(getattr(self, 'show')):
+            self.show()
+
+    @abstractmethod
+    def show(self):
+        pass
+
+
+class DefaultPage(Page):
+
+    def show(self):
+        raise NotImplementedError(
+            f'You need to override "show" method in {self.__class__.__name__} class.')
+
+
+class SelectionPage(Page):
+    def __init__(self):
+        super().__init__()
+
+    def show():
         pass
