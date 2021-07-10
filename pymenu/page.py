80d79593a88be7e80ee08cc3267275908e090752
Lik Eduard <feft99@gmail.com>
2021-07-10 22:10:55 +0600

    create PageCollector, - DefaultPage...

--- pymenu/page.py
+++ pymenu/page.py
@@ -1,28 +1,61 @@
 from __future__ import annotations
 import typing as t
 from abc import ABCMeta, abstractmethod
+from pymenu.context import Context
 
 
 class Page(metaclass=ABCMeta):
-    def __init__(self):
-        if hasattr(self, 'show') and callable(getattr(self, 'show')):
-            self.show()
+    def __init__(self, context: t.Type[Context]):
+        self.context: t.Type[Context] = context
+        # if hasattr(self, 'show') and callable(getattr(self, 'show')):
+        #     self.show()
 
     @abstractmethod
     def show(self):
         pass
 
+# class Page:
+#     def __init__(self, context: t.Type[Context]):
+#         self.context: t.Type[Context] = context
 
-class DefaultPage(Page):
-
-    def show(self):
-        raise NotImplementedError(
-            f'You need to override "show" method in {self.__class__.__name__} class.')
+#     def show(self):
+#         raise NotImplementedError(
+#             f'"show" method in {self.__class__.__name__} class must be overriden.')
 
 
 class SelectionPage(Page):
-    def __init__(self):
-        super().__init__()
+    def show(self):
+        pass
 
-    def show():
+
+class PageCollector:
+    """ Class which manages decorated classes inherited 
+    from `~pymenu.page.Page`."""
+
+    def __init__(self):
+        # {page_name: page_class}
+        self._pages: t.Dict[str, t.Type[Page]] = {}
+
+    def add(self, page_name: str, page_class: t.Type[Page]) -> None:
+        self._pages[page_name] = page_class
+
+    def has_page_name(self, page_name: str):
+        if page_name in self._pages.keys():
+            return True
+        return False
+
+    def has_page_class(self, page_class: t.Type[Page]):
+        if page_class in self._pages.values():
+            return True
+        return False
+
+    @property
+    def page_names(self) -> t.Tuple[str]:
+        return tuple(self._pages.keys())
+
+    @property
+    def page_classes(self) -> t.Tuple[t.Type[Page]]:
+        return tuple(self._pages.values())
+
+    def _collect_info(self) -> None:
         pass
