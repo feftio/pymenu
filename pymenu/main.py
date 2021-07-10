bf227e073a2e48ca0840700ea7f9d881dbc8a293
Lik Eduard <feft99@gmail.com>
2021-07-11 02:04:41 +0600

    rename + refact main.py

--- pymenu/main.py
+++ pymenu/main.py
@@ -1,37 +1,38 @@
 from __future__ import annotations
+from pymenu.holder import Holder
 import typing as t
-from pymenu.page import Page, PageCollector
+from pymenu.page import Page
 from pymenu.context import Context
 
 
 class PyMenu:
 
     def __init__(self, separator: t.Optional[str] = None, *args, **kwargs) -> None:
-        # self.context: t.Type[Context] = Context()
-        self.page_collector: t.Type[PageCollector] = PageCollector()
+        self.context: t.Type[Context] = Context()
+        self.pageholder: t.Type[Holder] = Holder()  # {pagename: pageclass}
         self.separator: t.Optional[str] = separator
 
     @property
-    def pagenames(self):
-        return self.page_collector.page_names
+    def pages(self):
+        return tuple(map(lambda item: (item[0], item[1].__name__), self.pageholder.items()))
 
-    def run(self, name: str) -> None:
-        if not self.page_collector.has_page_name(name):
+    def run(self, name: str) -> t.Optional[str]:
+        if not self.pageholder.haskey(name):
             return None
-        page = self.page_collector._pages[name]
+        page = self.pageholder[name]
         page(Context()).show()
-        return page.__class__
+        return page.__name__
 
     def page(self, name: str) -> t.Callable:
-        def decorator(page_class: t.Type[Page]):
-            if not isinstance(page_class, Page.__class__):
+        def decorator(pageclass: t.Type[Page]):
+            if not isinstance(pageclass, Page.__class__):
                 raise TypeError(
-                    f'{page_class.__name__} must be inherited from Page class.')
-            self.page_collector.add(name, page_class)
-            return page_class
+                    f'{pageclass.__name__} must be inherited from Page class.')
+            self.pageholder.add(name, pageclass)
+            return pageclass
         return decorator
 
     def item(self, output: str, triggers: t.List[str]) -> t.Callable:
-        def decorator(item_function: t.Callable):
-            return item_function
+        def decorator(f: t.Callable):
+            return f
         return decorator
