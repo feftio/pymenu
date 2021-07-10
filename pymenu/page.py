c2fcb09129926c002fc46e95d40429f349d5f42a
Lik Eduard <feft99@gmail.com>
2021-07-11 01:26:40 +0600

    delete PageCollector from page.py

--- pymenu/page.py
+++ pymenu/page.py
@@ -26,36 +26,3 @@ class Page(metaclass=ABCMeta):
 class SelectionPage(Page):
     def show(self):
         pass
-
-
-class PageCollector:
-    """ Class which manages decorated classes inherited 
-    from `~pymenu.page.Page`."""
-
-    def __init__(self):
-        # {page_name: page_class}
-        self._pages: t.Dict[str, t.Type[Page]] = {}
-
-    def add(self, page_name: str, page_class: t.Type[Page]) -> None:
-        self._pages[page_name] = page_class
-
-    def has_page_name(self, page_name: str):
-        if page_name in self._pages.keys():
-            return True
-        return False
-
-    def has_page_class(self, page_class: t.Type[Page]):
-        if page_class in self._pages.values():
-            return True
-        return False
-
-    @property
-    def page_names(self) -> t.Tuple[str]:
-        return tuple(self._pages.keys())
-
-    @property
-    def page_classes(self) -> t.Tuple[t.Type[Page]]:
-        return tuple(self._pages.values())
-
-    def _collect_info(self) -> None:
-        pass
