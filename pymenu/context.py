df0a5a4164d505d516b4abd6a6c613e78042f266
Lik Eduard <feft99@gmail.com>
2021-07-18 19:11:19 +0600

    create ContextManager class in context.py

--- pymenu/context.py
+++ pymenu/context.py
@@ -1,8 +1,20 @@
-class Context:
+from __future__ import annotations
+import typing as t
 
+
+class Context:
     def __init__(self) -> None:
         pass
-    
+
     @property
     def page(self) -> str:
         return 'routemap'
+
+
+class ContextManager:
+    def __init__(self, context: t.Type[Context]):
+        self.context: t.Type[Context] = context
+
+
+if __name__ == '__main__':
+    context_manager = ContextManager()
