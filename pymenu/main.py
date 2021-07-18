80c5c696a26dd18644413c4c733f1d0fd8f33302
Lik Eduard <feft99@gmail.com>
2021-07-19 05:28:21 +0600

    delete itemholder, item() + added args, kwargs for run()

--- pymenu/main.py
+++ pymenu/main.py
@@ -10,18 +10,17 @@ class PyMenu:
     def __init__(self, separator: t.Optional[str] = None, *args, **kwargs) -> None:
         self.context: t.Type[Context] = Context()
         self.pageholder: t.Type[Holder] = Holder()  # {pagename: pageclass}
-        self.itemholder: t.Type[Holder] = Holder()  # {pagename: [<Callable>]}
         self.separator: t.Optional[str] = separator
 
     @property
     def pages(self):
         return tuple(map(lambda item: (item[0], item[1].__name__), self.pageholder.items()))
 
-    def run(self, name: str) -> t.Optional[str]:
+    def run(self, name: str, *args, **kwargs) -> t.Optional[str]:
         if not self.pageholder.haskey(name):
             return None
         page = self.pageholder[name]
-        page(Context()).show()
+        page(Context()).show(*args, **kwargs)
         return page.__name__
 
     def page(self, name: str) -> t.Callable:
@@ -31,9 +30,4 @@ class PyMenu:
                     f'{pageclass.__name__} must be inherited from Page class.')
             self.pageholder.add(name, pageclass)
             return pageclass
-        return decorator
-
-    def item(self, output: str, triggers: t.List[str]) -> t.Callable:
-        def decorator(f: t.Callable):
-            return f
         return decorator
