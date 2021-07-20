b839aac77dfb7c5173334cbc5205deb7d61119d7
Lik Eduard <feft99@gmail.com>
2021-07-20 15:39:55 +0600

    New example.py

--- examples/example.py
+++ examples/example.py
@@ -7,31 +7,40 @@ import os
 menu = PyMenu()
 
 
-@menu.page(name='home')
-class HomeSelectionPage(SelectionPage):
-    @menu.item(output='1. Home.', triggers=['1'])
-    def home(self):
-        pass
+# @menu.page(name='home')
+# class HomeSelectionPage(SelectionPage):
+#     @menu.item(output='1. Home.', triggers=['1'])
+#     def home(self):
+#         pass
 
-    # @menu.item(output='2. Other.', triggers=['2'])
-    # def other(self):
-    #     pass
+#     @menu.item(output='2. Other.', triggers=['2'])
+#     def other(self):
+#         pass
 
-    # @menu.item(output='3. Exit.', triggers=['3'])
-    # def exit(self):
-    #     os.system('exit')
+#     @menu.item(output='3. Exit.', triggers=['3'])
+#     def exit(self):
+#         os.system('exit')
 
 
-# @menu.page(name='about')
+# @menu.page('about')
 # class AboutPage(Page):
 #     def show(self):
 #         print('About Page is showing...')
 
 
-# @menu.page(name='other')
+# @menu.page('other')
 # class OtherPage(Page):
 #     def show(self):
-#         print('Other Page is showing...')
+#         pass
 
-# menu.run('other')
-menu.run('home')
+@menu.page('some')
+class SomePage(Page):
+    def choose_algorithm(self):
+        pass
+
+    def helper(self):
+        pass
+
+    def show(self):
+        self.item(label='1. Choose algorithm.', trigger=self.choose_algorithm, on=['1'])
+        self.item(label='2. Help.', trigger=self.helper)
