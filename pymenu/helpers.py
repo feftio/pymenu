0683337f27fe94710e24e1b0c85bf95fd477920e
Eduard Lik <feft99@gmail.com>
2021-07-22 03:20:09 +0600

    Add "protect" helper

--- pymenu/helpers.py
+++ pymenu/helpers.py
@@ -1,3 +1,24 @@
+from __future__ import annotations
+import typing as t
+
+
+def protect(*protected) -> t.Type[Protect]:
+    """Returns a metaclass that protects all attributes given as strings"""
+    class Protect(type):
+        has_base = False
+
+        def __new__(meta, name, bases, attrs):
+            if meta.has_base:
+                for attribute in attrs:
+                    if attribute in protected:
+                        raise AttributeError(
+                            f'Overriding of attribute "{attribute}" not allowed.')
+            meta.has_base = True
+            klass = super().__new__(meta, name, bases, attrs)
+            return klass
+    return Protect
+
+
 def redirect():
     pass
 
