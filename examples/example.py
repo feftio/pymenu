4d5aa067dbad2368e155d703342e03fdfc3d40fe
Eduard Lik <feft99@gmail.com>
2021-07-22 21:43:29 +0600

    Modify example.py

--- examples/example.py
+++ examples/example.py
@@ -7,42 +7,18 @@ import os
 menu = PyMenu()
 
 
-# @menu.page(name='home')
-# class HomeSelectionPage(SelectionPage):
-#     @menu.item(output='1. Home.', triggers=['1'])
-#     def home(self):
-#         pass
-
-#     @menu.item(output='2. Other.', triggers=['2'])
-#     def other(self):
-#         pass
-
-#     @menu.item(output='3. Exit.', triggers=['3'])
-#     def exit(self):
-#         os.system('exit')
+@menu.page('home')
+class HomePage(Page):
+    def build(self):
+        self.item(label='1. Choose algorithm.')
+        self.item(label='2. Help.')
 
 
-# @menu.page('about')
-# class AboutPage(Page):
-#     def show(self):
-#         print('About Page is showing...')
-
-
-# @menu.page('other')
-# class OtherPage(Page):
-#     def show(self):
-#         pass
-
-@menu.page('some')
+@menu.page('work')
 class SomePage(Page):
-    def choose_algorithm(self):
-        pass
+    def build(self):
+        self.item(label='1. Run "Exit".')
+        self.item(label='2. Back.')
 
-    def helper(self):
-        pass
 
-    def show(self):
-        self.item(label='1. Choose algorithm.', trigger=self.choose_algorithm, on=['1'])
-        self.item(label='2. Help.', trigger=self.helper)
-
-menu.run('some')
+menu.run('work')
