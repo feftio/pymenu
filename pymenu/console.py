ff9d4048a9203aa7956e068737f4545215e162af
Eduard Lik <feft99@gmail.com>
2021-07-22 21:43:15 +0600

    Created class Console with 2 static methods in console.py

---
+++ pymenu/console.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+import typing as t
+import os
+
+
+class Console:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def listen() -> None:
+        return input()
+
+    @staticmethod
+    def clear():
+        os.system('cls' if os.name == 'nt' else 'clear')
