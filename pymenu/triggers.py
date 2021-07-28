167e0d2fb5f2d3ba8ab1f38c898771dfee954192
Eduard Lik <feft99@gmail.com>
2021-07-29 05:22:30 +0600

    Create Trigger class with childs

---
+++ pymenu/triggers.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+import typing as t
+
+
+class Trigger:
+    pass
+
+
+class CharsTrigger(Trigger):
+    pass
+
+
+class KeysTrigger(Trigger):
+    pass
+
+
+class MouseTrigger(Trigger):
+    pass
