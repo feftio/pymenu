fcba6d4730538c037dff58630b98579ee2501213
Eduard Lik <feft99@gmail.com>
2021-07-29 18:49:00 +0600

    Refactoring elements.py

--- pymenu/elements.py
+++ pymenu/elements.py
@@ -1,35 +1,45 @@
 from __future__ import annotations
-from pymenu.triggers import Trigger
 import typing as t
-from pymenu.listener import Listener
+from abc import ABC, abstractmethod
+from pymenu.listener import GroupListener, Listener
+from pymenu.triggers import Trigger
 
 
-class Element(Listener):
+class ElementInterface(ABC):
+    @abstractmethod
     def render(self) -> None:
-        raise NotImplementedError(
-            f'You need to override "render" method in {self.__class__.__name__} class.')
+        pass
+
+
+class Element(ElementInterface, Listener):
+    pass
+
+
+class GroupElement(ElementInterface, GroupListener):
+    def __init__(self, childs: t.Tuple[ElementInterface]):
+        self.childs = childs
+
+
+class Group(GroupElement):
+    def __init__(self, *childs: t.Tuple[ElementInterface]):
+        super().__init__(childs=childs)
+
+    def render(self) -> None:
+        for element in self.childs:
+            element.render()
 
 
 class Item(Element):
-    def __init__(self, label: str, action: t.Callable, triggers: t.Tuple[Trigger]):
-        self.label: t.Optional[str] = label
+    def __init__(self, label: str = '', action: t.Optional[t.Callable] = None, triggers: t.Optional[Trigger] = None):
+        self.label: str = label
         self.listener(action, triggers)
 
     def render(self) -> None:
         print(self.label)
 
 
-class Group(Element):
-    def __init__(self, *elements: Element):
-        self.elements: t.Tuple[Element] = elements
-
-    def render(self) -> None:
-        for element in self.elements:
-            element.render()
-
-
 class Back(Item):
-    def __init__(self, label: str, triggers: t.Tuple[Trigger]):
+    def __init__(self, label: str, triggers: t.Optional[t.Tuple[Trigger]] = None):
         super().__init__(label, self._action, triggers)
 
     def _action(self) -> None:
@@ -37,7 +47,7 @@ class Back(Item):
 
 
 class Hidden(Element):
-    def __init__(self, action: t.Callable, triggers: t.Tuple[Trigger]):
+    def __init__(self, action: t.Optional[t.Callable] = None, triggers: t.Optional[t.Tuple[Trigger]] = None):
         self.listener(action, triggers)
 
     def render(self) -> None:
