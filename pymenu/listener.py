cd2360f48dcb8049d3b72441dc68fdfcb5d84120
Eduard Lik <feft99@gmail.com>
2021-07-29 18:47:55 +0600

    Create GroupListener, make action, triggers optional + make if in listen method

--- pymenu/listener.py
+++ pymenu/listener.py
@@ -4,9 +4,26 @@ from pymenu.triggers import Trigger
 
 
 class Listener:
-    def listener(self, action: t.Callable, triggers: t.Tuple[Trigger]) -> None:
-        self.action: t.Callable = action
-        self.triggers: t.Tuple[Trigger] = triggers
+    def listener(self, action: t.Optional[t.Callable] = None, triggers: t.Optional[t.Tuple[Trigger]] = None) -> None:
+        self.action: t.Optional[t.Callable] = action
+        self.triggers: t.Optional[t.Tuple[Trigger]] = triggers
 
     def listen(self, trigger: Trigger) -> None:
-        pass
+        if ((not hasattr(self, 'action'))
+                or (not hasattr(self, 'triggers'))
+                or (getattr(self, 'action') is None)
+                or (getattr(self, 'triggers') is None)
+                ):
+            return
+        # TODO: if trigger in triggers, execute action.
+
+
+class GroupListener(Listener):
+    def _notify(self, trigger: Trigger) -> None:
+        """ Must be inherated with class which consist childs fields. """
+        for element in self.childs:
+            element.listen(trigger)
+
+    def listen(self, trigger: Trigger) -> None:
+        super().listen(trigger)
+        self._notify(trigger)
