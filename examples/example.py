541d2db7d718754cadaee8526b72594a154a3dd3
Eduard Lik <feft99@gmail.com>
2021-07-31 19:31:01 +0600

    Modify example.py

--- examples/example.py
+++ examples/example.py
@@ -1,6 +1,6 @@
 from pymenu import PyMenu
 from pymenu.page import Page
-from pymenu.elements import Group, Item, Back, Hidden
+from pymenu.elements import Group, Item, Back, Hidden, Redirect
 from pymenu.triggers import CharsTrigger, KeysTrigger
 
 
@@ -18,12 +18,17 @@ class HomePage(Page):
         return Group(
             Item(
                 label='1. First.',
+                triggers=('1'),
+                action=lambda: menu.run('work')
             ),
             Item(
-                label='2. Second.',
+                label='2. Print history.',
+                triggers=('2', 'print'),
+                action=lambda: print(self.context.history)
             ),
-            Back(
-                label='3. Third.',
+            Redirect(
+                to='work',
+                triggers=()
             ),
             Hidden(
                 action=lambda: print('Hidden called...'),
@@ -32,4 +37,19 @@ class HomePage(Page):
         )
 
 
-menu.run('home')
+@menu.page('work')
+class WorkPage(Page):
+    def build(self):
+        return Group(
+            Item(
+                label='1. Write report.',
+                triggers=('1', 'write', '@Ctrl + v'),
+                action=lambda: menu.run('home')
+            ),
+            Back(
+                label='2. Go back.',
+            )
+        )
+
+
+menu.run('work')
