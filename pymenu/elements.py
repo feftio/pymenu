d786e2f60eab67b3fe0037aac5ed59091911c42f
Eduard Lik <feft99@gmail.com>
2021-07-31 19:36:56 +0600

    Add ListenerInterface, modify elements

--- pymenu/elements.py
+++ pymenu/elements.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
 import typing as t
 from abc import ABC, abstractmethod
-from pymenu.listener import GroupListener, Listener
+from pymenu.listener import GroupListener, Listener, ListenerInterface
 from pymenu.triggers import Trigger
 
 
-class ElementInterface(ABC):
+class ElementInterface(ListenerInterface, ABC):
     @abstractmethod
     def render(self) -> None:
         pass
@@ -22,7 +22,7 @@ class GroupElement(ElementInterface, GroupListener):
 
 class Group(GroupElement):
     def __init__(self, *childs: t.Tuple[ElementInterface]):
-        super().__init__(childs=childs)
+        super().__init__(childs)
 
     def render(self) -> None:
         for element in self.childs:
@@ -30,20 +30,15 @@ class Group(GroupElement):
 
 
 class Item(Element):
-    def __init__(self, label: str = '', action: t.Optional[t.Callable] = None, triggers: t.Optional[Trigger] = None):
-        self.label: str = label
+    # TODO: make "label" optional.
+    def __init__(self, label: t.Optional[str] = None, action: t.Optional[t.Callable] = None, triggers: t.Optional[Trigger] = None):
+        self.label: t.Optional[str] = label
         self.listener(action, triggers)
 
     def render(self) -> None:
+        if self.label is None:
+            return
         print(self.label)
-
-
-class Back(Item):
-    def __init__(self, label: str, triggers: t.Optional[t.Tuple[Trigger]] = None):
-        super().__init__(label, self._action, triggers)
-
-    def _action(self) -> None:
-        pass
 
 
 class Hidden(Element):
@@ -51,4 +46,21 @@ class Hidden(Element):
         self.listener(action, triggers)
 
     def render(self) -> None:
+        pass
+
+
+class Redirect(Item):
+    def __init__(self, to: str, label: t.Optional[str] = None, triggers: t.Optional[t.Tuple[Trigger]] = None):
+        super().__init__(label, self._action, triggers)
+        self.to = to
+
+    def _action(self) -> None:
+        pass
+
+
+class Back(Item):
+    def __init__(self, label: t.Optional[str] = None, triggers: t.Optional[t.Tuple[Trigger]] = None):
+        super().__init__(label, self._action, triggers)
+
+    def _action(self) -> None:
         pass
