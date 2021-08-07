4187d7c899ba9006ffd2f45eee4ad9c8128396b0
Eduard Lik <feft99@gmail.com>
2021-08-07 22:23:26 +0600

    Delete ContextThrower, modify type of self.history of Context class

--- pymenu/context.py
+++ pymenu/context.py
@@ -1,24 +1,11 @@
 from __future__ import annotations
 import typing as t
-from pymenu.holder import Holder
 
 
 class Context:
     def __init__(self) -> None:
-        self.history: t.List = []
+        self.history: t.List[str] = []
 
     @property
     def page(self) -> str:
         return 'routemap'
-
-
-class ContextThrower:
-    def __init__(self, context: t.Type[Context]):
-        self.context: t.Type[Context] = context
-
-    def throw(self, holder: t.Type[Holder]) -> None:
-        pass
-
-
-if __name__ == '__main__':
-    context_manager = ContextThrower().throw()
